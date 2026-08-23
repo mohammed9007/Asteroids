c5dd17b068035e6561261c8aefb6f278af86cd79
mohammed9007 <mohammeddada9007@gmail.com>
2026-08-23 18:29:14 +0300

    L3 done

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    
+   pygame.init()
+   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+ 
    while True:
-       log_state()
+       
        for event in pygame.event.get():
            if event.type == pygame.QUIT:       
               return
           
        screen.fill("black")  
        pygame.display.flip()
+       log_state()
         
 if __name__ == "__main__":
     main()
