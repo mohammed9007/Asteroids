1e1a2bffc4776d04137849ab6fe2a81f7732b419
mohammed9007 <mohammeddada9007@gmail.com>
2026-08-24 19:51:55 +0300

    CH3 L2

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 
 
 def main():
-   pygame.init()
-   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   clock = pygame.time.Clock()
-   dt = 0.0
-   while True:
-       
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:       
-              return
-          
-       screen.fill("black")  
-       pygame.display.flip()
-       clock.tick(60)
-       dt = clock.tick(60) / 1000
-       log_state()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0.0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         
+        
+
+        screen.fill("black")
+        player1.draw(screen)
+        pygame.display.flip()
+   
+        dt = clock.tick(60) / 1000
+
+        log_state()
+
+
 if __name__ == "__main__":
     main()
